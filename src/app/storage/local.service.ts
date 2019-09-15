810996f8d62b05ce504c81396a01cae473eab465
Blaise <blaze.kalonda@gmail.com>
2019-09-15 04:46:14 +0200

    action column ok token list to finish

--- src/app/storage/local.service.ts
+++ src/app/storage/local.service.ts
@@ -8,6 +8,8 @@ import {Idata} from '../interface/idata';
 })
 export class LocalService {
 
+liste = [];
+
   constructor() {
   }
 
@@ -15,14 +17,23 @@ export class LocalService {
 
     const localStorageItems = JSON.parse(localStorage.getItem('data'));
 
-    return localStorageItems == null ? [{TokenName: 'TTISM' , TokenTicker: 'TTT',
-    TotalSupply: 10000, CreationDate: '17 mai 2019', IssuerName: 'Taurus group', Template: 'ERC20', Country: 'Switzerland'}]
-     : localStorageItems.data ;
+    return localStorageItems == null ? {TokenName: 'TTISM' , TokenTicker: 'TTT',
+    TotalSupply: 100000, CreationDate: '17 mai 2019', IssuerName: 'Taurus group', Template: 'ERC20', Country: 'Switzerland'}
+  : localStorageItems.data ;
 
   }
 
   public postData(data: Idata): void {
-    localStorage.setItem('data', JSON.stringify({data}));
-    }
+
+    this.liste.push(data);
+    let local = [];
+    let datas = [];
+    datas = [{...this.getData()}];
+    local = datas.concat(this.liste);
+    localStorage.setItem('data', JSON.stringify(local));
+
+    console.log('service:', local);
+
+  }
 
 }
--- src/app/token-list/token-list.component.ts
+++ src/app/token-list/token-list.component.ts
@@ -9,18 +9,24 @@ import { LocalService } from '../storage/local.service';
 })
 export class TokenListComponent  {
 
-  public data: Idata;
+        public data: Idata;
 
-  public iterable = [];
+        public iterable = [];
+        newIterable = [];
 
-  constructor(localService: LocalService) {
-    this.data = localService.getData();
-    this.iterable = [{...this.data}];
-  }
-clearLocalStorage() {
-  localStorage.clear();
-  location.reload();
-}
+        constructor(localService: LocalService) {
+          this.data = localService.getData();
+
+           this.iterable = [{...this.data}];
+          // this.newIterable = this.iterable.map(el => el.data !== {} ? {...el}: 0 );
+
+          console.log('iterable', this.iterable);
+
+        }
+      clearLocalStorage() {
+        localStorage.clear();
+        //location.reload();
+      }
 
 }
 
